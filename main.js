254b5d25b4a478f87f53f611a9b39f66fd86c934
blade202 <blade28202@gmail.com>
2022-02-01 14:55:55 +0100

    Create main.js

--- main.js
+++ main.js
@@ -1,48 +1,16 @@
-fetch('http://example.com/movies.json')
-  .then(response => response.json())
-  .then(data => console.log(data));
-
-function post()
- {
-    fetch('http://example.com/movies.json',
-    {
-        method:'POST',
-        body:JSON.stringify({
-            title:'postTitle',
-            body:'postBody',
-            userId:'0'
-        }),
-        Headers:{
-            "Content-type":"application/json; charset:UTF-8"
-        },
-    })
-    .then(res => res.json())
-    .then(data=> console.log(data));
+async function getBoors()
+{
+    let response =await fetch("https://api.trello.com/1/boards/{2}?key={36264967cdb14a5dc6a509bfda14da42}&token={ede4a3490af328d05c28d33d459e02bccdf7ac06657ecc13e9f2e44d367ec3e3}'");
+    let data=await response.json();
+    console.log(data);
+    show(data);
     
- }
+}
 
- function put()
- {
-    fetch('http://example.com/movies.json',
-    {
-        method:'PUT',
-        body:JSON.stringify({
-            title:'putTitle',
-            body:'putBody',
-            userId:'1'
-        }),
-        Headers:{
-            "Content-type":"application/json; charset:UTF-8"
-        },
-    })
-    .then(res=> res.json())
-    .then(data=>console.log(data));
- }
-
- function delete()
- {
-    fetch('http://example.com/movies.json',
-    {
-        method:'DELETE'
-    })
- }
+function show(data)
+{
+        for (const item of data) {
+            console.log(item);
+        }
+}
+getBoors();
